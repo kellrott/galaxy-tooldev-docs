02f59ee956cab58db4780658436a88f4175146b4
Kyle Ellrott <kellrott@gmail.com>
2015-03-19 14:57:17 -0700

    Working on the synapse push

--- synapse_push.py
+++ synapse_push.py
@@ -5,6 +5,7 @@ import yaml
 import os
 import re
 import cgi
+import json
 
 PROJECT_ID = "syn2786217"
 WIKI_BASE  = "74384"
@@ -47,6 +48,14 @@ def markdown_clean(txt, encoding="utf-8"):
     return txt.encode(encoding)
 
 
+def load_doc(page):
+    print "loading", page
+    page_path = os.path.join("docs", page)
+    with open(page_path) as handle:
+        markdown = handle.read()
+    new_text = markdown_clean(markdown)
+    return new_text
+
 if __name__ == "__main__":
     syn = synapseclient.Synapse()
     syn.login()
@@ -56,43 +65,51 @@ if __name__ == "__main__":
         if c.get('parentId', '') == WIKI_BASE:
             child_pages[c['title']] = c['id']
 
+    print child_pages
+
     with open('mkdocs.yml') as handle:
         doc_config = yaml.load(handle.read())
 
-    #load the index
-    index_path = os.path.join("docs", doc_config['pages'][0][0])
-    with open(index_path) as handle:
-        markdown = handle.read()
-    new_text = markdown_clean(markdown)
-    #compare to what is online
-    wiki = syn.getWiki(PROJECT_ID, WIKI_BASE)
-    if new_text != wiki.markdown:
-        wiki.markdown = new_text
-        syn.store(wiki)
-    else:
-        print "Skipping Page Update"
+    index = doc_config['pages'][0][0]
+    index_title = "%s - %s" % (SECTION_BASE, doc_config['pages'][0][1])
+    src = {
+         index_title : {
+            'doc' : load_doc(doc_config['pages'][0][0]),
+            'wiki_id' : WIKI_BASE
+        }
+    }
+
+    sections = []
+    for page_num, page in enumerate(doc_config['pages'][1:]):
+        if page[1] not in sections:
+            sections.append(page[1])
+
+    section_numbers = {}
+    for i, s in enumerate(sections):
+        section_numbers[s] = i
 
     #Load/Update other pages
     for page_num, page in enumerate(doc_config['pages'][1:]):
-        page_path = os.path.join("docs", page[0])
-        with open(page_path) as handle:
-            markdown = handle.read()
-        new_text = markdown_clean(markdown)
-        title = page[1]
-        page_title = "%s.%s - %s" % (SECTION_BASE, page_num, title)
-        if page_title not in child_pages:
-            print "creating new Page:", page_title
-            wiki = synapseclient.wiki.Wiki(title=page_title,
+        page_title = "%s.%s.%s - %s" % (SECTION_BASE, section_numbers[page[1]], page_num, page[2])
+        src[page_title] = {
+            'doc' : load_doc(page[0]),
+            'wiki_id' : child_pages.get(page_title, None)
+        }
+
+    #print json.dumps(src, indent=4)
+    for title, entry in src.items():
+        if entry['wiki_id'] is None:
+            print "creating new Page:", title
+            wiki = synapseclient.wiki.Wiki(title=title,
                 owner=PROJECT_ID,
                 parentWikiId=WIKI_BASE,
-                markdown=new_text)
+                markdown=entry['doc'])
             syn.store(wiki)
         else:
-            wiki = syn.getWiki(PROJECT_ID, child_pages[page_title])
-            print page
-            if new_text != wiki.markdown:
-                print "Updating Page", page
-                wiki.markdown = new_text
+            wiki = syn.getWiki(PROJECT_ID, entry['wiki_id'])
+            if entry['doc'] != wiki.markdown:
+                print "Updating Page", title
+                wiki.markdown = entry['doc']
                 syn.store(wiki)
             else:
-                print "Skipping Page Update", page
+                print "Skipping Page Update", title
